a423cea3757a8641adb5e7997ab5a6925254ba82
jaeyson <hello@jaeyson.dev>
2026-02-03 14:51:20 +0800

    add live_debugger

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule RecordVerifier.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:live_debugger, "~> 0.5", only: [:dev]},
       {:ash_admin, "~> 0.13"},
       {:ash_authentication_phoenix, "~> 2.0"},
       {:picosat_elixir, "~> 0.2"},
